4f2f6c1731a751531ccb46dd8bf507c0f4a71d0c
jan <janek.grochulski@gmail.com>
2021-11-03 10:56:08 -0500

    fin

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 
+function superbowlWin(array) {
+    const finder = array.find(element => element.result === "W");
+    if (finder != undefined) {
+        return finder.year;
+    }
+    return undefined
+}
